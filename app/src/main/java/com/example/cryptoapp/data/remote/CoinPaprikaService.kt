532d1b58b2076959f88b9e0d22fa87f92eb71dd3
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2022-04-08 19:02:00 -0400

    set up coin models and response objects and hook up to api

---
+++ app/src/main/java/com/example/cryptoapp/data/remote/CoinPaprikaService.kt
@@ -0,0 +1,17 @@
+package com.example.cryptoapp.data.remote
+
+import com.example.cryptoapp.data.remote.dto.CoinDetailResponse
+import com.example.cryptoapp.data.remote.dto.CoinResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinPaprikaService {
+
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinResponse>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(
+        @Path("coinId") coinId: String
+    ): CoinDetailResponse
+}
