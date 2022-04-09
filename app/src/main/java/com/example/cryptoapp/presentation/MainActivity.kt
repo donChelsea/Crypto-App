0bdb292d2f87048511d64096ba3e9632e021378b
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2022-04-09 03:11:30 -0400

    created ui for coin list screen

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.cryptoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptoapp.presentation.coinlist.CoinListScreen
 import com.example.cryptoapp.presentation.theme.CryptoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,9 +21,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                    }
                 }
             }
         }
