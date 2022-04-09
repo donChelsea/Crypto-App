523e4f6ac4cc936496f8ade956c0597ee00402a2
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2022-04-08 21:53:44 -0400

    set up hilt and dagger within the classes

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cryptoapp
+package com.example.cryptoapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cryptoapp.presentation.theme.CryptoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
